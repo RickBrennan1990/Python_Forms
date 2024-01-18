57345d2695e8247e275b09d924eb3cb36a99e40a
Patrick B <patrickbrennancoding@gmail.com>
2024-01-17 21:45:29 -1000

    added DOB to the Form

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, redirect, url_for
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
+from wtforms import StringField, SubmitField, RadioField, DateField, TextAreaField
 from wtforms.validators import DataRequired
 
 app = Flask(__name__)
@@ -8,7 +8,8 @@ app.config['SECRET_KEY'] = 'patricks_most_secret_key'  # Replace with a real sec
 
 class MyForm(FlaskForm):
     name = StringField('Name', validators=[DataRequired()])
-    submit = SubmitField('Submit')
+    submit = SubmitField('Submit', validators=[DataRequired()])
+    DOB = DateField('DOB')
 
 @app.route('/')
 def index():
@@ -19,8 +20,10 @@ def form():
     form = MyForm()
     if form.validate_on_submit():
         name = form.name.data
-        return f"Hello, {name}!"
+        DOB = form.DOB.data
+        return f"Hello, {name}, Your birthday is {DOB}!"
     return render_template('form.html', form=form)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
